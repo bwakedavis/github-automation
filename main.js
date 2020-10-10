7ad6c260949438bc9c351c09e51d6f36bf20ea2b
bwakedavis <bwakedavis@gmail.com>
2020-10-10 12:46:51 +0300

    added the jwt package and oauth token

--- main.js
+++ main.js
@@ -1,10 +1,40 @@
 let fs = require('fs');
+var GitHub = require('github-api');
+var jwt = require('jsonwebtoken');
 require('dotenv').config()
 
 
-let privatePem = fs.readFile('./kijaniyard.2020-10-05.private-key.pem', 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log(data);
-  });
+let PRIVATE_PEM = fs.readFileSync('public.pem'); 
+
+  // basic auth
+var gh = new GitHub({
+  
+     token: process.env.MY_OAUTH_TOKEN
+   
+});
+
+var me = gh.getUser(); // no user specified defaults to the user for whom credentials were provided
+// me.listNotifications(function(err, notifications) {
+//    // do some stuff
+//    console.log(notifications)
+// });
+
+me.getProfile(function(err, profile) {
+  // do some stuff
+  if(err){
+    console.log(err)
+  }else{
+    console.log(profile)
+  }
+  
+});
+
+me.listFollowers(function(err, followers) {
+  // do some stuff
+  if(err){
+    console.log(err)
+  }else{
+    console.log(followers)
+  }
+  
+});
